a680902fa7a7b382e578791fd8913034b862f528
patrick.bryant1 <patrick.bryant@live.com>
2025-05-15 13:05:32 +0200

    installatin

---
+++ install_dependencies.sh
@@ -0,0 +1,37 @@
+#Install conda env
+conda env create -f environment.yml
+
+wait
+conda activate rarefold
+pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+pip install --upgrade numpy
+conda deactivate
+
+## Get network parameters for Umol (a few minutes)
+#Pocket params
+wget /params20000.npy
+mkdir data/params
+mv params20000.npy  data/params/
+#No-pocket params
+wget /finetuned_params25000.npy
+mv finetuned_params25000.npy data/params/
+
+wait
+## Get Uniclust30 (10-20 minutes depending on bandwidth)
+# 25 Gb download, 87 Gb extracted
+wget http://wwwuser.gwdg.de/~compbiol/uniclust/2018_08/uniclust30_2018_08_hhsuite.tar.gz --no-check-certificate
+mkdir data/uniclust30
+mv uniclust30_2018_08_hhsuite.tar.gz data
+cd data
+tar -zxvf uniclust30_2018_08_hhsuite.tar.gz
+cd ..
+
+wait
+## Install HHblits (a few minutes)
+git clone https://github.com/soedinglab/hh-suite.git
+mkdir -p hh-suite/build && cd hh-suite/build
+cmake -DCMAKE_INSTALL_PREFIX=. ..
+make -j 4 && make install
+cd ../..
+
+wait
